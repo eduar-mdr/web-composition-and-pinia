bfc7f3b247bcaaf9568746b54b347bfbdb69cb05
Eduardo Medrano <87587568+eduar-mdr@users.noreply.github.com>
2024-07-12 00:02:34 -0600

    Composable Loadings: add selects loadings

--- src/composables/useLoaading.js
+++ src/composables/useLoaading.js
@@ -5,18 +5,34 @@ export function useLoadings(){
     const loadingButton = ref(false);
     const loadingContent = ref(false);
 
+    /* Select Loadings */
+    const loadingStudents = ref(false);
+    const loadingCourses = ref(false);
+
     //Loading functions
     function setLoadingButton(value) {
         loadingButton.value =  value
     }
-    //Loading functions
     function setLoadingContent(value) {
         loadingContent.value =  value
     }
+    //Loading functions for selects
+    function setLoadingStudents(value) {
+        loadingStudents.value =  value
+    }
+    function setLoadingCourses(value) {
+        loadingCourses.value =  value
+    }
+
     return {
         loadingButton,
         loadingContent,
         setLoadingButton,
         setLoadingContent,
+
+        loadingStudents,
+        loadingCourses,
+        setLoadingStudents,
+        setLoadingCourses,
     }
 }
